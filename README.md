449d0bac09d577d2caae4eae5b373eb19e6eaa41
Ryan Hirsch <ryan.hirsch@mixpanel.com>
2020-02-13 18:40:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# mixpanel-panel README
+
+A collection of snippets for creating Mixpanel Panel components
+
+## Features
+
+- Scaffold out a component
+
+## Requirements
+
+None
+
+## Release Notes
+
+### 0.0.1
+
+Trial build
---
+++ scripts/local-install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_DIR="$( cd "${SCRIPTS_DIR}" && cd .. && pwd )"
+EXTENSIONS_DIR="${HOME}/.vscode"
+
+if [[ -f "${PROJECT_DIR}/.env" ]]; then
+  set -o allexport
+  source "${PROJECT_DIR}/.env"
+  set +o allexport
+fi
+
+
+if [[ -d "${HOME}/.vscode-insiders" ]]
+then
+  VSCODE_DIR="${HOME}/.vscode-insiders"
+  echo "Installing to VS Code Insiders"
+else
+  if [[ -d "${HOME}/.vscode" ]]
+  then
+    VSCODE_DIR="${HOME}/.vscode"
+    echo "Installing to standard VS Code"
+  else
+    echo "Failed to find your VS Code install"
+    exit 1
+  fi
+fi
+
+yarn global add vsce
+
+cd "${PROJECT_DIR}"
+vsce package
+
+EXTENSION_DIR="${VSCODE_DIR}/extensions"
+
+cp "${PROJECT_DIR}/mixpanel-panel-0.0.1.vsix" "${EXTENSION_DIR}"
